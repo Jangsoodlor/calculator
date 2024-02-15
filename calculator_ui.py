216cb9e3b324ddb669788fe70aa6f8167ce904a7
Rattapoom Pethputong <celica2548@gmail.com>
2024-02-15 20:24:33 +0700

    add display component

--- calculator_ui.py
+++ calculator_ui.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 from tkinter import font
 from keypad import Keypad
+from display import Display
 
 class CalculatorUI(tk.Tk):
     """UI for calculator"""
@@ -11,25 +12,18 @@ class CalculatorUI(tk.Tk):
         self.title('Calc')
         self.init_components()
 
-    def keypress(self, event):
-        """Output the key that the user presses on the display"""
-        text = event.widget['text']
-        self.output.set(self.output.get() + text)
-
     def init_components(self):
         """init components"""
         options = {'fill': 'both', 'expand':True}
+        self.display = Display()
         self.default_font = font.nametofont('TkDefaultFont')
         self.default_font.configure(family='Arial', size=20, weight='bold')
-        num_frame = Keypad(self, list('789456123 0.'), columns=3)
-        num_frame.bind('<Button>', self.keypress)
-        op_frame = Keypad(self, ['*', '/','+','-','^','='], columns=1)
-        op_frame.bind('<Button>', self.keypress)
-        display = tk.Label(bg='black', fg='yellow',
-                           textvariable=self.output, anchor='e', font=('Arial', 50))
-        display.pack(side='top', **options, anchor='n')
-        num_frame.pack(**options, side='left', anchor='w')
-        op_frame.pack(**options, side='right', anchor='e')
+        num_pad= Keypad(self, list('789456123 0.'), columns=3)
+        operator_pad = Keypad(self, ['(',')','**','%','*', '/','+','-','=&colspan=2'], columns=2)
+
+        self.display.pack(side='top', **options)
+        num_pad.pack(**options, side='left')
+        operator_pad.pack(**options, side='right')
 
     def run(self):
         """keeps the program open"""
